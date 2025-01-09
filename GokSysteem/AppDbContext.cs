3fc320ce3751a4d9ba4a5f7ef652c5c53f54807a
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2025-01-09 10:16:36 +0100

    changes
    fix by stevan

---
+++ GokSysteem/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextDatabase
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                    "server=localhost;" +
+                    "port=3306;" +
+                    "user=root;" +
+                    "password=;" +
+                    "database=prac3",
+                    ServerVersion.Parse("8.0.30")
+                    );
+        }
+    }
+}
