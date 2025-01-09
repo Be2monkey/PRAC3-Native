dbf65d4451a98a5ae01327452b82c20636a86f73
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2025-01-09 13:10:43 +0100

    zomaar .vs bestanden
    e

--- obj/Release/net8.0/PRAC3.AssemblyInfo.cs
+++ obj/Release/net8.0/PRAC3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRAC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40349f6650a432b559b75facf923647361e6a414")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRAC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRAC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
